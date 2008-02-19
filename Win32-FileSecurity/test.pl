2890e51ff9f7014450861bf548725f7cd39d1700
Jan Dubois <jand@activestate.com>
2008-02-19 01:55:38 +0000

    Tests only work on NTFS [http://rt.cpan.org/Ticket/Display.html?id=33388]

--- Win32-FileSecurity/test.pl
+++ Win32-FileSecurity/test.pl
@@ -3,6 +3,7 @@ BEGIN { $| = 1; print "1..1\n"; }
 END {print "not ok 1\n" unless $loaded;}
 
 use Win32::FileSecurity qw(Get EnumerateRights);
+use Win32;
 
 $loaded = 1;
 print "ok 1\n";
@@ -10,6 +11,7 @@ print "ok 1\n";
 print STDERR "##### This will work only when files are in NTFS #####\n";
 
 foreach (<*>) {
+    last unless Win32::FsType =~ /^NTFS/;
     next unless -e $_;
     my(%hash) = ();
     if ( Get( $_, \%hash ) ) {
