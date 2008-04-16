17e55b526bc58932f434e8226dbba41a7b1f09ba
Jan Dubois <jand@activestate.com>
2008-04-16 00:00:38 +0000

    Prepare for CPAN upload: version => 1.05; update Changes

--- Win32-FileSecurity/FileSecurity.pm
+++ Win32-FileSecurity/FileSecurity.pm
@@ -13,7 +13,7 @@ require Exporter;
 require DynaLoader;
 use Carp ;
 
-$VERSION = '1.04';
+$VERSION = '1.05';
 
 require Win32 unless defined &Win32::IsWinNT;
 croak "The Win32::FileSecurity module works only on Windows NT" unless Win32::IsWinNT();
