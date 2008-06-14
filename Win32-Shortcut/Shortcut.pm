3f2339ecb387d61d8201fa41966e4b2406311850
Jan Dubois <jand@activestate.com>
2008-06-14 01:09:04 +0000

    Prepare for Win32-Shortcut-0.07 release today

--- Win32-Shortcut/Shortcut.pm
+++ Win32-Shortcut/Shortcut.pm
@@ -8,7 +8,7 @@ package Win32::Shortcut;
 #
 #######################################################################
 
-$VERSION = "0.06";
+$VERSION = "0.07";
 
 require Exporter;
 require DynaLoader;
